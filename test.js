7d587724b55d225e4db716dd974c635d1c43918a
Helena Ajaots <helenaajaots@Helenas-MacBook-Pro.local>
2025-07-15 17:31:20 +0300

    Add env variable test script

---
+++ test.js
@@ -0,0 +1,4 @@
+console.log("OPENAI_API_KEY:", process.env.OPENAI_API_KEY);
+console.log("SENDGRID_API_KEY:", process.env.SENDGRID_API_KEY);
+console.log("STRIPE_SECRET_KEY:", process.env.STRIPE_SECRET_KEY);
+
